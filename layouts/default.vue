d9f286552e211a126c3377e5c2e75a6f03303b10
summer2up <1501814239@qq.com>
2025-08-14 22:23:49 +0800

    设置Layout和首页布局

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="relative">
     <AppHeader class="sticky top-0 z-111 text-red-500" />
-    <NuxtPage/>
+      <NuxtPage/>
     <AppFooter />
   </div>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,17 +1,36 @@
 <template>
-  <div>
-    <nuxt-link to="/about" class="about">About</nuxt-link>
+  <div class="min-h-screen w-screen overflow-hidden">
+    <div class="home w-full h-full min-h-screen relative">
+      <div class="home-buttons h-[700px] w-[50px] flex flex-col justify-between p-5">
+        <div class="home-link mt-10">home</div>
+        <div class="home-music">
+          <ul>
+            <li>music</li>
+            <li>music</li>
+            <li>music</li>
+            <li>music</li>
+            <li>music</li>
+            <li>music</li>
+          </ul>
+        </div>
+      </div>
+      <div class="home-text absolute top-1/2 left-[20%]">文本们</div>
+    </div>
+    <div class="scroll-page absolute right-0 top-0 h-full w-[10%] text-center flex items-center justify-center">scroll</div>
   </div>
 </template>
-<script lang="ts" setup>
 
+<script setup>
+definePageMeta({
+  middleware: 'home',
+})
 </script>
 
 <style lang="scss">
-@use '@/assets/styles/variable.scss';
-
-.about {
-  color: $color;
-  font-size: $font-size;
+.container {
+  width: 100vw;
+  height: 100vh;
+  margin: 0;
+  padding: 0;
 }
 </style>
